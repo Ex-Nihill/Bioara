ece600cee6c3aa685fafc1e9d96ea91f550079e8
miguel de Oliveira <pigextreme.ban@gmail.com>
2026-09-02 14:58:14 -0300

    atualização css e router 02/09

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { SobreNos } from './features/sobre-nos/sobre-nos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'sobre-nos',
+    loadComponent: () => import('./features/sobre-nos/sobre-nos').then((m) => m.SobreNos),
+  },
+  {
+    path: 'Produtos',
+    loadComponent: () => import('./features/produtos/produtos').then((m) => m.Produtos),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./features/conta/conta').then((m) => m.Conta)
+  }
+  
+];
--- src/app/features/sobre-nos/sobre-nos.ts
+++ src/app/features/sobre-nos/sobre-nos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sobre-nos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sobre-nos.html',
   styleUrl: './sobre-nos.css',
 })
