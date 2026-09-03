161bfd21f6518be8429e3e87f27ae2a5673d5b49
miguel de Oliveira <pigextreme.ban@gmail.com>
2026-09-03 16:37:54 -0300

    Carrosel feito 03/09

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit, OnDestroy {
+  imagemAtual = 0;
+
+  imagens = ['image1.jpeg', 'image2.jpeg', 'image3.jpeg'];
+
+  private intervalo?: ReturnType<typeof setInterval>;
+
+  ngOnInit(): void {
+    this.iniciarCarrossel();
+  }
+
+  iniciarCarrossel(): void {
+    this.intervalo = setInterval(() => {
+      this.proxima();
+    }, 4000);
+  }
+
+  proxima(): void {
+    this.imagemAtual = (this.imagemAtual + 1) % this.imagens.length;
+  }
+
+  anterior(): void {
+    this.imagemAtual = (this.imagemAtual - 1 + this.imagens.length) % this.imagens.length;
+  }
+
+  irPara(index: number): void {
+    this.imagemAtual = index;
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalo) {
+      clearInterval(this.intervalo);
+    }
+  }
+}
