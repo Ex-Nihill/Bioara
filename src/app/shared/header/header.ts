6db27735b56e35c1458e38ccf128876f300113a2
nicollyalvarenga1-maker <nicollyalvarenga1@gmail.com>
2026-09-16 01:19:05 -0300

    pagina de esqueceua a senha

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -3,10 +3,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
+interface UsuarioLogado {
+  nome: string;
+  email: string;
+}
 @Component({
   selector: 'app-header',
   imports: [MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  getUsuarioLogado(): UsuarioLogado | null {
+    const dados = localStorage.getItem('usuarioLogado');
+    return dados ? JSON.parse(dados) : null;
+  }
+}
